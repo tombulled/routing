3d00408a970490e6f74148b4225e1dcd2108b3ac
Tom Bulled <26026015+tombulled@users.noreply.github.com>
2022-04-15 11:06:11 +0100

    Improve typing and test coverage (#3)

--- routing/api.py
+++ routing/api.py
@@ -1,5 +1,6 @@
 import dataclasses
 import inspect
+from typing import Any, Dict, List
 
 import annotate
 import mediate
@@ -7,46 +8,54 @@ import mediate
 from . import models, routers, sentinels
 
 
-def get_routes(obj, *, sep: str = ""):
-    annotations = annotate.get_annotations(obj)
+def get_routes(obj: Any, /) -> List[models.Route]:
+    if isinstance(obj, models.Route):
+        return [obj]
+
+    if isinstance(obj, routers.Router):
+        return obj.routes
+
+    annotations: Dict[Any, annotate.Annotation] = annotate.get_annotations(obj)
 
     if sentinels.Route in annotations:
         return [
-            dataclasses.replace(target=obj) for route in annotations[sentinels.Route]
+            dataclasses.replace(route, target=obj)
+            for route in annotations[sentinels.Route]
         ]
 
     return [
         dataclasses.replace(route, target=member)
-        for name, member in inspect.getmembers(obj)
+        for _, member in inspect.getmembers(obj)
         for route in annotate.get_annotations(member).get(sentinels.Route, ())
     ]
 
 
-def get_middleware(obj):
+def get_middleware(obj: Any, /) -> List[Any]:
     if sentinels.Middleware in annotate.get_annotations(obj):
         return [obj]
 
+    if isinstance(obj, routers.Router):
+        return obj.middlewares
+
     return [
         member
-        for name, member in inspect.getmembers(obj)
+        for _, member in inspect.getmembers(obj)
         if sentinels.Middleware in annotate.get_annotations(member)
     ]
 
 
-def router(*objs, sep: str = ""):
-    middlewares = mediate.Middleware()
+def router(*objs: Any) -> routers.Router:
+    middlewares: mediate.Middleware = mediate.Middleware()
+    routes: List[models.Route] = []
 
+    obj: Any
     for obj in objs:
-        for m in get_middleware(obj):
-            middlewares.append(m)
+        middleware: Any
+        for middleware in get_middleware(obj):
+            middlewares.append(middleware)
 
-    return routers.Router(
-        routes=[
-            route
-            for obj in objs
-            for route in (
-                [obj] if isinstance(obj, models.Route) else get_routes(obj, sep=sep)
-            )
-        ],
-        middlewares=middlewares,
-    )
+        route: models.Route
+        for route in get_routes(obj):
+            routes.append(route)
+
+    return routers.Router(routes=routes, middlewares=middlewares)
