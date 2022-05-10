94552311ab700460c7c8b958feef31e0900b34e4
Tom Bulled <26026015+tombulled@users.noreply.github.com>
2022-05-10 21:51:56 +0100

    Make Router generic by implementing AbstractRouter

--- routing/router.py
+++ routing/router.py
@@ -1,5 +1,5 @@
 import dataclasses
-from typing import Any, Callable, List, TypeVar
+from typing import Any, Callable, Generic, List, TypeVar
 
 from . import models
 from . import errors
@@ -12,11 +12,13 @@ import annotate
 
 
 T = TypeVar("T")
+R = TypeVar("R", bound=models.Route)
 
 
 @dataclasses.dataclass
-class Router:
-    routes: List[models.Route] = dataclasses.field(default_factory=list)
+class AbstractRouter(Generic[R]):
+    route_factory: Callable[..., R]
+    routes: List[R] = dataclasses.field(default_factory=list)
     middlewares: mediate.Middleware = dataclasses.field(
         default_factory=mediate.Middleware
     )
@@ -30,19 +32,19 @@ class Router:
 
         return self.handle(request)
 
-    def __getitem__(self, path: str, /) -> models.Route:
+    def __getitem__(self, path: str, /) -> R:
         return self.resolve(path)
 
     def __setitem__(self, path: str, target: Callable) -> None:
         self.routes.append(
-            models.Route(
+            self.route_factory(
                 path=path,
                 target=target,
             ),
         )
 
     def handle(self, request: models.Request) -> Any:
-        route: models.Route = self.resolve(request.path)
+        route: R = self.resolve(request.path)
 
         args: arguments.Arguments = route.match(request.path)
 
@@ -61,8 +63,8 @@ class Router:
 
         return target(request)
 
-    def resolve(self, path: str) -> models.Route:
-        route: models.Route
+    def resolve(self, path: str) -> R:
+        route: R
         for route in self.routes:
             if route.matches(path):
                 return route
@@ -73,7 +75,7 @@ class Router:
         def wrapper(target: Callable, /) -> Callable:
             path: str
             for path in paths:
-                route: models.Route = models.Route(path, **kwargs, target=target)
+                route: R = self.route_factory(path, **kwargs, target=target)
 
                 self.routes.append(dataclasses.replace(route))
 
@@ -91,11 +93,11 @@ class Router:
         def wrapper(obj: T, /) -> T:
             mount: models.Mount = models.Mount(*args, **kwargs)
 
-            routes: List[models.Route] = annotate.get_annotations(obj).get(
+            routes: List[R] = annotate.get_annotations(obj).get(
                 enums.Annotation.ROUTE, []
             )
 
-            route: models.Route
+            route: R
             for route in self.routes:
                 if route in routes:
                     route.path = mount.apply(route.path, sep=self.separator)
@@ -110,3 +112,11 @@ class Router:
         self.middlewares.append(obj)
 
         return obj
+
+
+@dataclasses.dataclass
+class Router(AbstractRouter[models.Route]):
+    route_factory: Callable[..., models.Route] = dataclasses.field(
+        default=models.Route, init=False, repr=False
+    )
+    routes: List[models.Route] = dataclasses.field(default_factory=list)
