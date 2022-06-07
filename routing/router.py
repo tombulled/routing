4f921e401534d87096e07e508ba34c99cac913c4
Tom Bulled <26026015+tombulled@users.noreply.github.com>
2022-06-07 21:37:48 +0100

    :sparkles: Implement Abstract Classes (#13)

--- routing/router.py
+++ routing/router.py
@@ -1,5 +1,6 @@
+from abc import ABC
 import dataclasses
-from typing import Any, Callable, Generic, List, TypeVar
+from typing import Any, Callable, Generic, List, Optional, TypeVar
 
 from . import models
 from . import errors
@@ -7,75 +8,28 @@ from . import enums
 from . import annotations
 
 import mediate
-import arguments
+from arguments import Arguments
 import annotate
 
 
-T = TypeVar("T")
-R = TypeVar("R", bound=models.Route)
+C = TypeVar("C", bound=Callable)
+RO = TypeVar("RO", bound=models.AbstractRoute)
+RE = TypeVar("RE", bound=models.AbstractRequest)
 
 
 @dataclasses.dataclass
-class AbstractRouter(Generic[R]):
-    route_factory: Callable[..., R]
-    routes: List[R] = dataclasses.field(default_factory=list)
+class AbstractRouter(Generic[RO, RE], ABC):
+    routes: List[RO] = dataclasses.field(default_factory=list)
     middlewares: mediate.Middleware = dataclasses.field(
         default_factory=mediate.Middleware
     )
     separator: str = ""
 
-    def __call__(self, path: str, *args: Any, **kwargs: Any) -> Any:
-        request: models.Request = models.Request(
-            path=path,
-            args=arguments.Arguments(*args, **kwargs),
-        )
-
-        return self.handle(request)
-
-    def __getitem__(self, path: str, /) -> R:
-        return self.resolve(path)
-
-    def __setitem__(self, path: str, target: Callable) -> None:
-        self.routes.append(
-            self.route_factory(
-                path=path,
-                target=target,
-            ),
-        )
-
-    def handle(self, request: models.Request) -> Any:
-        route: R = self.resolve(request.path)
-
-        args: arguments.Arguments = route.match(request.path)
-
-        request = dataclasses.replace(
-            request,
-            args=arguments.Arguments(*args, *request.args.args, **request.args.kwargs),
-        )
-
-        def process(request: models.Request) -> Any:
-            if route.target is None:
-                return None
-
-            return route.target(*request.args.args, **request.args.kwargs)
-
-        target: Callable[[models.Request], Any] = self.middlewares.compose(process)
-
-        return target(request)
-
-    def resolve(self, path: str) -> R:
-        route: R
-        for route in self.routes:
-            if route.matches(path):
-                return route
-
-        raise errors.RouteNotFound(repr(path))
-
-    def route(self, *paths: str, **kwargs: Any) -> Callable[[Callable], Callable]:
-        def wrapper(target: Callable, /) -> Callable:
-            path: str
-            for path in paths:
-                route: R = self.route_factory(path, **kwargs, target=target)
+    def build_route_registrar(self, routes: List[RO]) -> Callable[[C], C]:
+        def wrapper(target: C, /) -> C:
+            route: RO
+            for route in routes:
+                route = dataclasses.replace(route, target=target)
 
                 self.routes.append(dataclasses.replace(route))
 
@@ -89,15 +43,13 @@ class AbstractRouter(Generic[R]):
 
         return wrapper
 
-    def mount(self, *args, **kwargs) -> Callable[[T], T]:
-        def wrapper(obj: T, /) -> T:
-            mount: models.Mount = models.Mount(*args, **kwargs)
-
-            routes: List[R] = annotate.get_annotations(obj).get(
+    def build_mount_registrar(self, mount: models.Mount) -> Callable[[C], C]:
+        def wrapper(obj: C, /) -> C:
+            routes: List[RO] = annotate.get_annotations(obj).get(
                 enums.Annotation.ROUTE, []
             )
 
-            route: R
+            route: RO
             for route in self.routes:
                 if route in routes:
                     route.path = mount.apply(route.path, sep=self.separator)
@@ -106,7 +58,40 @@ class AbstractRouter(Generic[R]):
 
         return wrapper
 
-    def middleware(self, obj: T) -> T:
+    def __getitem__(self, path: str, /) -> List[RO]:
+        return [route for route in self.routes if route.matches(path)]
+
+    def apply(self, route: RO, request: RE) -> Any:
+        if route.target is None:
+            return None
+
+        return route.target(request.data)
+
+    def compose(self, route: RO) -> Callable[[RE], Any]:
+        def process(request: RE) -> Any:
+            return self.apply(route, request)
+
+        return self.middlewares.compose(process)
+
+    def handle(self, request: RE) -> Any:
+        route: RO = self.resolve(request.path)
+
+        return self.compose(route)(request)
+
+    def resolve(self, path: str) -> RO:
+        routes: List[RO] = self[path]
+
+        if not routes:
+            raise errors.RouteNotFound(path)
+
+        return routes[0]
+
+    def mount(self, *args, **kwargs) -> Callable[[C], C]:
+        mount: models.Mount = models.Mount(*args, **kwargs)
+
+        return self.build_mount_registrar(mount)
+
+    def middleware(self, obj: C) -> C:
         annotations.middleware(obj)
 
         self.middlewares.append(obj)
@@ -115,8 +100,27 @@ class AbstractRouter(Generic[R]):
 
 
 @dataclasses.dataclass
-class Router(AbstractRouter[models.Route]):
-    route_factory: Callable[..., models.Route] = dataclasses.field(
-        default=models.Route, init=False, repr=False
-    )
+class Router(AbstractRouter[models.Route, models.Request]):
     routes: List[models.Route] = dataclasses.field(default_factory=list)
+
+    def __call__(self, path: str, *args: Any, **kwargs: Any) -> Any:
+        return self.handle(models.Request(path, *args, **kwargs))
+
+    def apply(self, route: models.Route, request: models.Request) -> Any:
+        if route.target is None:
+            return None
+
+        args: Optional[Arguments] = route.match(request.path)
+
+        if args is not None:
+            request = dataclasses.replace(
+                request,
+                data=Arguments(*args, *request.data.args, **request.data.kwargs),
+            )
+
+        return route.target(*request.data.args, **request.data.kwargs)
+
+    def route(self, path: str) -> Callable[[C], C]:
+        route: models.Route = models.Route(path=path, target=None)
+
+        return self.build_route_registrar([route])
